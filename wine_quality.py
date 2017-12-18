d48c99e9ab5d6318a7cb0d77fdcd70c72b4e87bb
Nairit Banerjee <nairit32@gmail.com>
2017-12-18 23:13:10 +0530

    Updated code uploaded

--- wine_quality.py
+++ wine_quality.py
@@ -26,5 +26,19 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 pipeline = make_pipeline(preprocessing.StandardScaler(), RandomForestRegressor(n_estimators=100))
 
 #Declaring Hyperparameters
+hyperparameters = { 'randomforestregressor__max_features' : ['auto', 'sqrt', 'log2'],'randomforestregressor__max_depth': [None, 5, 3, 1]}
 
+#Cross-validation, runs the loop on pipeline data set to cross validate the hyperparameters and divides set into cv=10 parts
+clf = GridSearchCV(pipeline, hyperparameters, cv=10)
+ 
+# Fit and tune model
+clf.fit(X_train, y_train) #Refit is true by default
 
+#Evaluate with test data	
+y_pred = clf.predict(X_test)
+
+#Saving model for future use
+joblib.dump(clf, 'rf_regressor.pkl')
+#we can use this model later by writing folowing code:
+#clf2 = joblib.load('rf_regressor.pkl')
+#clf2.predict(X_test)
