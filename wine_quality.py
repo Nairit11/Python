bca370fa09c9bbc0342c15dade1ad3abd67ca0e3
Nairit Banerjee <nairit32@gmail.com>
2017-12-18 19:53:53 +0530

    Updated code uploaded

--- wine_quality.py
+++ wine_quality.py
@@ -17,5 +17,14 @@ y = data.quality
 X = data.drop('quality', axis=1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123, stratify=y)
 
+#Pre-processing with Transformer API
+#scaler = preprocessing.StandardScaler().fit(X_train)
+#X_train_scaled = scaler.transform(X_train)
+#X_test_scaled = scaler.transform(X_test)  #transforming test set from means of training set
+
+#Instead we will directly model a pipeline like this
+pipeline = make_pipeline(preprocessing.StandardScaler(), RandomForestRegressor(n_estimators=100))
+
+#Declaring Hyperparameters
 
 
