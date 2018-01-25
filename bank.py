01ab78b18c0eaba60e51cc437493cb18126b6ed3
Nairit Banerjee <nairit32@gmail.com>
2018-01-25 19:46:23 +0000

    Linear Regression example

--- bank.py
+++ bank.py
@@ -1,3 +1,5 @@
+# Importing required packages
+
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
@@ -9,4 +11,9 @@ import seaborn as sns
 sns.set(style="white")
 sns.set(style="whitegrid", color_codes=True)
 
+# Importing the Dataset
+data=pd.read_csv('~/Documents/bank/bank.csv',header=0)
+data=data.dropna()
+#print(data.head())
 
+print(data['education'].unique())
