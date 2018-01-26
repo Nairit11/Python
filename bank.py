3585150ad79bcb79f568610b0b0577746a162b18
Nairit Banerjee <nairit32@gmail.com>
2018-01-26 06:58:10 +0000

    Add files via upload

--- bank.py
+++ bank.py
@@ -16,4 +16,15 @@ data=pd.read_csv('~/Documents/bank/bank.csv',header=0)
 data=data.dropna()
 #print(data.head())
 
-print(data['education'].unique())
+#print(data['education'].unique())
+#Too many categories for education, so we group some of them
+data['education']=np.where(data['education'] =='basic.9y', 'Basic', data['education'])
+data['education']=np.where(data['education'] =='basic.6y', 'Basic', data['education'])
+data['education']=np.where(data['education'] =='basic.4y', 'Basic', data['education'])
+
+#To view distribution of values of y
+sns.countplot(x='y',data=data,palette='hls')
+plt.show()
+
+#To display average values of the various features for all possible values of y
+print(data.groupby('y').mean())
